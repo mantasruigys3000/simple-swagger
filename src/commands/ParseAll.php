ff05f6566f746617ff73055ff77af8dca30a2448
mantasruigys3000 <mantasruigys101@gmail.com>
2025-05-28 21:59:15 +0100

    messy command

--- src/commands/ParseAll.php
+++ src/commands/ParseAll.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace Mantasruigys3000\SimpleSwagger\commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Foundation\Http\FormRequest;
+use Mantasruigys3000\SimpleSwagger\attributes\interfaces\ResponseAttribute;
+use Mantasruigys3000\SimpleSwagger\attributes\ResponseJson;
+use Mantasruigys3000\SimpleSwagger\attributes\ResponseResource;
+use Mantasruigys3000\SimpleSwagger\helpers\ReferenceHelper;
 use Mantasruigys3000\SimpleSwagger\helpers\RouteHelper;
 use Mantasruigys3000\SimpleSwagger\parser\ArrayKeyResolver;
 use Mantasruigys3000\SimpleSwagger\parser\ResourceKeyParser;
+use Mantasruigys3000\SimpleSwagger\traits\HasRequestBodies;
 use PhpParser\ConstExprEvaluator;
 use PhpParser\NodeDumper;
 use PhpParser\NodeFinder;
@@ -15,6 +21,7 @@ use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
 use PhpParser\ParserFactory;
 use PHPUnit\Event\Code\ClassMethod;
+use ReflectionAttribute;
 use ReflectionClass;
 use ReflectionException;
 use PhpParser\Node;
@@ -31,9 +38,12 @@ class ParseAll extends Command
     {
         $this->info('Parsing all files....');
 
+
+        $requestClasses = [];
+        $responseClasses = [];
+
         // Loop through registered routes
-        foreach (RouteHelper::getRoutes() as $route)
-        {
+        foreach (RouteHelper::getRoutes() as $route) {
             $controllerClass = $route->getControllerClass();
             $functionName = $route->getActionMethod();
 
@@ -41,15 +51,51 @@ class ParseAll extends Command
                 $functionName = '__invoke';
             }
 
-            $method = new ReflectionMethod($controllerClass,$functionName);
-            dump($method->name);
+            $method = new ReflectionMethod($controllerClass, $functionName);
 
+            // Get request method attribute
+            $methodParams = $method->getParameters();
+
+            if (count($methodParams) > 0) {
+                $requestParam = $methodParams[0]->getType()->getName();
+
+                if (is_subclass_of($requestParam, FormRequest::class)) {
+
+                    $traits = class_uses($requestParam);
+                    if (in_array(HasRequestBodies::class, $traits)) {
+                        $requestClasses[$requestParam] = 1;
+                    }
+                }
+            }
+
+            // Get all response classes
+            $responseAttributes = $method->getAttributes(ResponseAttribute::class, ReflectionAttribute::IS_INSTANCEOF);
+            foreach ($responseAttributes as $responseAttribute) {
+
+
+                // TODO do not want to update this every time a new case is added
+
+                /**
+                 * @var ResponseAttribute $responseObject
+                 */
+                $responseObject = $responseAttribute->newInstance();
+                if ($responseObject instanceof ResponseResource) {
+                    $responseClasses[$responseObject->resourceClass] = 1;
+                }
+
+                if ($responseObject instanceof ResponseJson) {
+                    $responseClasses[$responseObject->jsonResponse] = 1;
+                }
+            }
         }
+
+        dd($requestClasses,$responseClasses);
         // Compare all requests
 
         // Compare all responses
 
         // Final output
     }
+
 
 }
